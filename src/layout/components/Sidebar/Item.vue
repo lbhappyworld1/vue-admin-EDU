5bc29d696fd9bb00cf43014e5ba14b3546016e2a
liubin <103818443@qq.com>
2020-06-09 15:32:08 +0800

    code commit

---
+++ src/layout/components/Sidebar/Item.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+  name: 'MenuItem',
+  functional: true,
+  props: {
+    icon: {
+      type: String,
+      default: ''
+    },
+    title: {
+      type: String,
+      default: ''
+    },
+    icontype: {
+      type: String,
+      default: 'svg'
+    }
+  },
+  render(h, context) {
+    const { icon, title, icontype } = context.props
+    const vnodes = []
+    if (icontype === 'svg') {
+      if (icon) {
+        vnodes.push(<svg-icon icon-class={icon}/>)
+      }
+    }
+    if (icontype === 'el') {
+      if (icon) {
+        vnodes.push(<div class='el-item'><i class={icon}/></div>)
+      }
+    }
+    if (title) {
+      vnodes.push(<span slot='title'>{(title)}</span>)
+    }
+    return vnodes
+  }
+}
+</script>
